e3ed7893a26a19d866a9f47c38e398b0abbee6a0
Ben McClung <aurumvalian@gmail.com>
2020-05-27 17:17:24 -0600

    Create constructor for activity class

--- src/activity.js
+++ src/activity.js
@@ -1,1 +1,10 @@
-
+class Activity {
+  constructor(category, description, minutes, seconds, id) {
+    this.category = category;
+    this.description = description;
+    this.minutes = minutes;
+    this.seconds = seconds;
+    this.id = id;
+    this.completed = false;
+  }
+}
