22f93ff5f4df13c1f2152f4aead7d66f823476dc
Ben McClung <aurumvalian@gmail.com>
2020-05-28 16:47:54 -0600

    Add event listener and function for delegation

--- src/main.js
+++ src/main.js
@@ -9,3 +9,17 @@ var inputGoalText = document.querySelector("#input-goal-text");
 var inputMinutesText = document.querySelector("#input-minutes-text");
 var inputSecondsText = document.querySelector("#input-seconds-text");
 var startActivityButton = document.querySelector(".start-activity-button");
+
+newActivitiesView.addEventListener('click', checkEventTarget);
+
+checkEventTarget(event) {
+  if (event.target === studyButton) {
+    placeholder();
+  } else if (event.target === meditateButton) {
+    placeholder();
+  } else if (event.target === exerciseButton) {
+    placeholder();
+  } else if (event.target === startActivityButton) {
+    placeholder2();
+  }
+}
