e5e800f87da0f3d635b73df325e796536d35cffa
Ben McClung <aurumvalian@gmail.com>
2020-05-28 16:36:18 -0600

    Add view and button query selector variables

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-
+var newActivitiesView = document.querySelector(".new-activities-view");
+var studyButton = document.querySelector(".study-button");
+var meditateButton = document.querySelector(".meditate-button");
+var exerciseButton = document.querySelector(".exercise-button");
+var studyIcon = document.querySelector("#study-icon");
+var meditateIcon = document.querySelector("#meditate-icon");
+var exerciseIcon = document.querySelector("#exercise-icon");
