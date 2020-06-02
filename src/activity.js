ffc9f69ee1368feb0be0ac053d33246fcc52374f
Leigh Larson <leighlarson4@gmail.com>
2020-06-02 16:11:12 -0600

    Tiny changes css, rename func noMultipleStarts in both js files

--- src/activity.js
+++ src/activity.js
@@ -2,40 +2,42 @@ class Activity {
   constructor(category, description, minutes, seconds) {
     this.category = category;
     this.description = description;
-    this.minutes = JSON.parse(minutes);
-    this.seconds = JSON.parse(seconds);
+    this.minutes = parseInt(minutes);
+    this.seconds = parseInt(seconds);
     this.id = Date.now();
     this.completed = false;
   }
 
   startTimer() {
     this.started = true;
-    var countdownEL = document.getElementById("timer");
+    var countdown = document.getElementById("timer");
     var counting = setInterval(function() {
-      countdownEL.innerText = `${currentActivity.minutes}:${currentActivity.seconds < 10 ? "0" + currentActivity.seconds : currentActivity.seconds}`;
+      countdown.innerText = `${currentActivity.minutes}:${currentActivity.seconds < 10 ? "0" + currentActivity.seconds : currentActivity.seconds}`;
+      document.querySelector(".start-timer-button").innerText = "In Progress";
       currentActivity.seconds--;
-      if (currentActivity.minutes <= 0 && currentActivity.seconds <= -1) {
+      if (currentActivity.minutes <= 0 && currentActivity.seconds === -1) {
         clearInterval(counting);
-        alert("Congratulations!");
-      }
-      if (currentActivity.seconds === -1) {
+        alert("Time's up! Activity complete.");
+      } else if (currentActivity.seconds === -1) {
         currentActivity.seconds = 59;
         currentActivity.minutes--;
       }
     }, 1000);
   }
 
-  singleRunValidation() {
-    if (!currentActivity.started) {
+  noMultipleStarts() {
+    if (!this.started) {
       currentActivity.startTimer();
     }
   }
 
   markComplete() {
     this.completed = true;
+    clearInterval(counting);
+    alert("Time's up! Activity complete.");
   }
 
   saveToStorage() {
-
+    
   }
 }
